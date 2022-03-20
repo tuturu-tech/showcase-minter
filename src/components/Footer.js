14670ed22ecfc33965df41200572e6c72e0958b1
tuturu-tech <9058533+tuturu-tech@users.noreply.github.com>
2022-03-20 16:18:10 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import content from "../content";
 
 const Footer = () => {
   return (
-    <div className="relative z-10 top-0 right-0 bg-[#25279e] h-28 pt-2">
-      <div className="flex flex-row justify-between items-center w-10/12 h-full mx-auto text-white">
+    <div className="relative z-10 top-0 right-0 bg-[#25279e] h-28 pt-2 pb-10">
+      <div className="flex flex-col md:flex-row justify-between items-center w-10/12 h-full mx-auto text-white">
         <p className="font-bold">Baby Boss</p>
-        <div className="flex flex-row h-full items-center justify-center">
+        <div className="flex flex-col md:flex-row h-full items-center justify-center">
           <div className="flex flex-row mr-10">
             {content.nav.links.map((item, index) => (
               <p key={index} className="p-3 text-sm">
