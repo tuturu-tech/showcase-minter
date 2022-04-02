8379d6ac00b7009c3fd913f6cd8b8d74a8bf53cc
tuturu-tech <elvis.skozdopolj@gmail.com>
2022-04-03 01:17:46 +0200

    testnet

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const { _contractConfig: configOverrides } = window;
 export const config = {
   maxSupply: 3999,
   purchaseLimit: 10,
-  erc721Address: "0x41978f599F793Bd4f697E3eAD1f5FB62BC5BcFC9", //"0x41978f599F793Bd4f697E3eAD1f5FB62BC5BcFC9", // "0x2DF5fDa90A29dED58c01eC5C8B1AB9113Fb990AF", // Change to correct address
+  erc721Address: "0x2DF5fDa90A29dED58c01eC5C8B1AB9113Fb990AF", //"0x41978f599F793Bd4f697E3eAD1f5FB62BC5BcFC9", // "0x2DF5fDa90A29dED58c01eC5C8B1AB9113Fb990AF", // Change to correct address
   chainId: 4,
   validChainname: "Rinkeby Testnet",
   ...configOverrides,
